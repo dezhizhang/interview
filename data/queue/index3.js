94d0caa4a07651f3ac7af5d6c35520d9c42c0090
zhangdezhi <zhangdezhi@inman.cc>
2021-02-27 15:25:01 +0800

    时间与空间复杂度

---
+++ data/queue/index3.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: your name
+ * @Date: 2021-02-27 14:51:19
+ * @LastEditTime: 2021-02-27 14:51:55
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /interview/data/queue/index3.js
+ */
+
+
+ setTimeout(() => console.log(1),0);
+ console.log(2);
+ 
