b7b429ab0ac72fcdb93df880a30c78b6b48eae79
zhangdezhi <zhangdezhi@inman.cc>
2021-02-27 19:05:32 +0800

    set集合

---
+++ data/set/index2.js
@@ -0,0 +1,33 @@
+/*
+ * @Author: your name
+ * @Date: 2021-02-27 18:53:50
+ * @LastEditTime: 2021-02-27 19:03:11
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /interview/data/set/index2.js
+ */
+
+//  let mySet = new Set()
+//  mySet.add(1);
+//  mySet.add(5);
+//  mySet.add(5);
+//  mySet.add("some text");
+
+//  let has = mySet.has(5);
+//  console.log(has);
+//  let res = mySet.delete(5);
+//  console.log("res",res);
+//  console.log(mySet);
+
+
+//  for(let key of mySet) {
+//      console.log(key);
+//  }
+ 
+//  console.log(mySet);
+
+
+let myArr = [...new Set([1])];
+
+console.log(myArr);
+
---
+++ data/set/index3.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: your name
+ * @Date: 2021-02-27 19:05:23
+ * @LastEditTime: 2021-02-27 19:05:23
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /interview/data/set/index3.js
+ */
