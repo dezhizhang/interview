41701a6b520048a16c51e934e1243d7814fb32c9
dezhizhang <1541609448@qq.com>
2021-02-28 11:41:04 +0800

    时间堆

--- data/pile/index.js
+++ data/pile/index.js
@@ -1,8 +1,14 @@
 /*
  * @Author: your name
  * @Date: 2021-02-28 11:34:42
- * @LastEditTime: 2021-02-28 11:34:42
+ * @LastEditTime: 2021-02-28 11:39:58
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /interview/data/pile/index.js
  */
+
+
+ //js中通用数组表示堆
+ //1,左侧子节点的位置2 * index + 1
+ //2,右则子节点的位置2 * index + 2
+ //3,父节点的位置(index - 1) / 2
