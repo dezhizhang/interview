f6536efc2373429ced6212e11bd38f51d9118acb
dezhizhang <1541609448@qq.com>
2021-03-03 23:42:12 +0800

    数组去重

---
+++ questions/day04/README.md
@@ -0,0 +1,16 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-03-03 23:34:02
+ * @LastEditTime: 2021-03-03 23:34:29
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /interview/questions/day04/README.md
+-->
+### 数组去重方法一
+```
+ let arr = [12,23,12,15,25,23,25,14,16];
+ let arr1 = Array.from(new Set(arr));
+ console.log(arr1);
+ 
+ ```
+ 
--- questions/day04/index.js
+++ questions/day04/index.js
@@ -1,8 +1,16 @@
 /*
  * @Author: your name
  * @Date: 2021-03-03 23:18:04
- * @LastEditTime: 2021-03-03 23:18:04
+ * @LastEditTime: 2021-03-03 23:36:33
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /interview/questions/day04/index.js
  */
+
+
+// let arr = [12,23,12,15,25,23,25,14,16];
+// let arr1 = Array.from(new Set(arr));
+// console.log(arr1);
+
+
+ 
