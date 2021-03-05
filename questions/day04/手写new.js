4b37f4d375947fa86133416c1c53651ca2d14a44
dezhizhang <1541609448@qq.com>
2021-03-05 23:22:23 +0800

    数组合并

--- questions/day04/手写new.js
+++ questions/day04/手写new.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-03-05 22:21:48
- * @LastEditTime: 2021-03-05 23:03:59
+ * @LastEditTime: 2021-03-05 23:20:46
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /interview/questions/day04/手写new.js
@@ -12,24 +12,30 @@ function Dog(name) {
     this.name = name;
 }
 
-Dog.prototype.brak = function() {
-    console.log("wangwang");
+Dog.prototype.break = function() {
+    console.log('break');
 }
+
 Dog.prototype.sayName = function() {
-    console.log(`this is name ${this.name}`);
+    console.log('this is a '+this.name);
 }
 
 function _new(Fn,...args) {
-    let obj = {}
+    let obj = {};
     obj.__proto__ = Fn.prototype;
     Fn.call(obj,...args);
     return obj;
 }
 
-let d = _new(Dog,"张三");
-d.brak();
+let d = _new(Dog,"hello");
+d.break();
 d.sayName();
 console.log(d instanceof Dog);
+
+
+
+
+
 
 
 
---
+++ questions/day04/数组合并.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: your name
+ * @Date: 2021-03-05 23:22:11
+ * @LastEditTime: 2021-03-05 23:22:12
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /interview/questions/day04/数组合并.js
+ */
