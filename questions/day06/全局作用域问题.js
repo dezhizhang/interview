f55fa7563f7f9e0cd478e6320e1b0b68f8952347
dezhizhang <1541609448@qq.com>
2021-03-06 22:34:54 +0800

    全局作用域问题

--- questions/day06/全局作用域问题.js
+++
@@ -1,8 +0,0 @@
-/*
- * @Author: your name
- * @Date: 2021-03-06 22:29:39
- * @LastEditTime: 2021-03-06 22:29:40
- * @LastEditors: Please set LastEditors
- * @Description: In User Settings Edit
- * @FilePath: /interview/questions/day06/全局作用域问题.js
- */
