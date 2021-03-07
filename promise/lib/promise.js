48a40bc1522df135b23c6a95662360da54daea34
dezhizhang <1541609448@qq.com>
2021-03-07 20:30:07 +0800

    自定认promise

---
+++ promise/lib/promise.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: your name
+ * @Date: 2021-03-07 20:27:31
+ * @LastEditTime: 2021-03-07 20:29:42
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /interview/promise/lib/promise.js
+ */
+
+
+(function (params){
+    
+})();
