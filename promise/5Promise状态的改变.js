de78c46646e4104462a61aebd30753aeed91b3f8
dezhizhang <1541609448@qq.com>
2021-03-07 15:13:11 +0800

    promise

---
+++ promise/5Promise状态的改变.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: your name
+ * @Date: 2021-03-07 14:53:15
+ * @LastEditTime: 2021-03-07 15:12:14
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /interview/promise/5Promise状态的改变.js
+ */
+
+ 
---
+++ questions/day07/index.js
@@ -0,0 +1,58 @@
+/*
+ * @Author: your name
+ * @Date: 2021-03-07 14:19:18
+ * @LastEditTime: 2021-03-07 14:35:06
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /interview/questions/day07/index.js
+ */
+
+
+//  console.log(null === null)
+
+// console.log(Infinity + 1 !== Infinity)
+
+// function a() {
+//     setTimeout(function() {
+//         console.log(1);
+//     },1)
+
+//     setTimeout(function() {
+//         console.log(2)
+//     },0)
+//     console.log(3)
+// }
+
+// a()
+
+// 3
+
+
+// var name = "Jay"
+// function Person(name) {
+//     this.name = name;
+//     console.log(this.name)
+// }
+
+// let a = Person("Tom")
+// console.log(name)
+// console.log(a)
+// var b = new Person('Micheel')
+// console.log(b);
+
+// {
+//     let a = 1
+// }
+// var b = a;
+
+let [a,b,c,d,e] = "hello";
+console.log(a);
+
+
+
+
+
+
+// // 'Jay'
+// 'Tom'
+// 'Micheel'
