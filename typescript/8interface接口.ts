cc9a8dec31de03e4f2887f792f1db48aaa261390
dezhizhang <1541609448@qq.com>
2021-03-08 21:38:16 +0800

    接口类型

--- typescript/8interface接口.ts
+++ typescript/8interface接口.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-03-08 21:21:20
- * @LastEditTime: 2021-03-08 21:33:06
+ * @LastEditTime: 2021-03-08 21:36:11
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /interview/typescript/8interface接口.ts
@@ -9,17 +9,30 @@
 
 
 
- interface User{
+// interface User{
+//     name:string
+//     age:string
+//     sex?:number
+
+// }
+
+
+// const userInfo = (user:User) => {
+//     console.log(user.name)
+//     console.log(user.age);
+// }
+
+// userInfo({name:'hello',age:"22"})
+
+interface User{
     name:string
     age:string
     sex?:number
+}
 
- }
+const userInfo = (user:User) => {
+    console.log(user.name);
+    console.log(user.age);
+}
 
-
- const userInfo = (user:User) => {
-     console.log(user.name)
-     console.log(user.age);
- }
-
- userInfo({name:'hello',age:"22"})
+userInfo({name:"hello",age:"22"});
---
+++ typescript/9interface接口.ts
@@ -0,0 +1,8 @@
+/*
+ * @Author: your name
+ * @Date: 2021-03-08 21:36:46
+ * @LastEditTime: 2021-03-08 21:36:46
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /interview/typescript/9interface接口.ts
+ */
