210a276476aa2bb206a8aa2262c4352331b35c0a
dezhizhang <1541609448@qq.com>
2021-03-08 22:37:42 +0800

    类的访问类型

--- typescript/11类的访问类型.ts
+++ typescript/11类的访问类型.ts
@@ -1,8 +1,21 @@
 /*
  * @Author: your name
  * @Date: 2021-03-08 22:10:53
- * @LastEditTime: 2021-03-08 22:10:54
+ * @LastEditTime: 2021-03-08 22:36:54
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /interview/typescript/11类的访问类型.ts
  */
+
+
+class Peron{
+    public name:string
+    public say() {
+        return this.name
+    }
+}
+
+let p = new Peron();
+p.name = "helo";
+let res = p.say()
+console.log('res',res);
