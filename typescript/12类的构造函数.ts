0526c58ff4f31c2a2878a739d6b6f73b1912aed7
dezhizhang <1541609448@qq.com>
2021-03-08 22:49:45 +0800

    编译项目

---
+++ typescript/12类的构造函数.ts
@@ -0,0 +1,28 @@
+/*
+ * @Author: your name
+ * @Date: 2021-03-08 22:39:09
+ * @LastEditTime: 2021-03-08 22:46:01
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /interview/typescript/12类的构造函数.ts
+ */
+
+
+ class Person{
+    // name:string;
+    // age:number;
+    constructor(public name:string,public age:number) {
+        this.name = name;
+        this.age = age;
+    }
+ }
+
+ class Teacher extends Person {
+     constructor(public name:string,public age:number) {
+         super(name,age);
+     }
+ }
+
+ const t = new Teacher("zhang",20);
+ console.log(t);
+ 
