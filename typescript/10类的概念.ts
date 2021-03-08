2a28c3dd8a9d2ccff758cac74c0eadfdb79b5e15
dezhizhang <1541609448@qq.com>
2021-03-08 21:52:51 +0800

    接口

---
+++ typescript/10类的概念.ts
@@ -0,0 +1,8 @@
+/*
+ * @Author: your name
+ * @Date: 2021-03-08 21:52:40
+ * @LastEditTime: 2021-03-08 21:52:41
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /interview/typescript/10类的概念.ts
+ */
