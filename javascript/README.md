1dc6a919e7390a0c751243ee5b193631d64990af
dezhizhang <1541609448@qq.com>
2021-03-13 01:08:09 +0800

    提交

---
+++ javascript/README.md
@@ -0,0 +1,8 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-03-12 23:34:16
+ * @LastEditTime: 2021-03-12 23:34:16
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /interview/javascript/README.md
+-->
---
+++ javascript/iife/index.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: your name
+ * @Date: 2021-03-12 23:34:03
+ * @LastEditTime: 2021-03-12 23:34:03
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /interview/javascript/iife/index.js
+ */
