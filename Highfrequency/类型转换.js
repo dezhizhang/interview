944b0c85a70e89eed74147089abab2822230ea46
dezhizhang <1541609448@qq.com>
2021-03-12 16:45:40 +0800

    提交

---
+++ Highfrequency/类型转换.js
@@ -0,0 +1,22 @@
+/*
+ * @Author: your name
+ * @Date: 2021-03-12 16:39:44
+ * @LastEditTime: 2021-03-12 16:45:10
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /interview/Highfrequency/类型转换.js
+ */
+
+ //字符串拼接
+//  const a = 100 + 10;
+//  const b = 100 + '10';
+//  const c = true + '10';
+
+//  console.log(a);
+//  console.log(b);
+//  console.log(c);
+
+// console.log(100 == '100');
+// console.log(0 == )
+
+ 
