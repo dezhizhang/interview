1486b8b2484b1b793d1dfe5ddb5e7ffa22e8caec
dezhizhang <1541609448@qq.com>
2021-03-13 20:54:47 +0800

    some方法

--- lodash/自定义方法.js
+++ lodash/自定义方法.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-03-13 20:17:02
- * @LastEditTime: 2021-03-13 20:41:34
+ * @LastEditTime: 2021-03-13 20:54:12
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /interview/lodash/自定义方法.js
@@ -39,16 +39,67 @@ Array.prototype.filter = function(callback) {
     return arr;
 }
 
+Array.prototype.find = function(callback) {
+    for(let i=0;i < this.length;i++) {
+        const item = this[i];
+        const bool = callback(item,i);
+        if(bool) {
+            return item;
+        }
+        // return bool ? item:undefined
+    }
+    return undefined;
+}
+
+
+Array.prototype.findIndex = function(callback) {
+    for(let i=0;i < this.length;i++) {
+        const item = this[i];
+        const bool = callback(item,i);
+        if(bool) {
+            return i
+        }
+    }
+    return -1;
+}
+
+Array.prototype.every = function(callback) {
+    for(let i=0;i < this.length;i++) {
+        const item = this[i];
+        const bool = callback(item,i);
+        if(!bool) {
+            return false;
+        }
+    }
+    return true;
+}
+
+Array.prototype.some = function(callback) {
+    for(let i=0;i < this.length;i++) {
+        const item = this[i];
+        const bool = callback(item,i);
+        if(bool) {
+            return true;
+        }
+    }
+    return false;
+}
 
 const arr = [1,3,6,9,15,19,16];
 
 const res = arr.map(item => item + 10);
 const r = arr.reduce((prevTot,item) => prevTot + item,0);
 const f = arr.filter(item => item > 6);
+const find = arr.find(item => item === 15);
+const findIndex = arr.findIndex(item => item === 15);
 
-console.log('res',res);
-console.log('r',r);
-console.log('f',f);
+
+
+// console.log('res',res);
+// console.log('r',r);
+// console.log('f',f);
+// console.log('find',find);
+
 
 
 
