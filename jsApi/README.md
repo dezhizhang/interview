0fd3f77a018242badd86a12bdfadab2100e70760
dezhizhang <1541609448@qq.com>
2024-04-18 07:41:43 +0800

    feat:宏任务

--- jsApi/README.md
+++ jsApi/README.md
@@ -1,1 +1,30 @@
 # 原生jsApi
+
+### Promise
+1. await 相当于Promise.then 处理不了Promise.reject
+2. 
+```js
+!(async function() {
+    const p4 = Promise.reject('err1');
+    const d = await p4;
+    console.log('data',d);
+})()
+
+```
+
+### 微任务执行时机比宏任务时机早
+1. 宏任务 setInterval,setTimeout,Ajax,Dom
+2. 微任务 async/await
+```js
+console.log(100);
+
+setTimeout(() => {
+    console.log(200);
+});
+
+Promise.resolve().then(() => {
+    console.log(300);
+});
+
+console.log(400);
+```
--- jsApi/src/index.js
+++ jsApi/src/index.js
@@ -1,10 +1,50 @@
 /*
- * :file description: 
+ * :file description:
  * :name: /jsApi/src/index.js
  * :author: 张德志
  * :copyright: (c) 2024, Tungee
  * :date created: 2024-04-18 05:51:29
  * :last editor: 张德志
- * :date last edited: 2024-04-18 05:52:28
+ * :date last edited: 2024-04-18 07:39:17
  */
-console.log('hello')
+// async function fn1() {
+//     return 100;
+// }
+
+// const res1 = fn1();
+// console.log('res1',res1);
+
+// !(async function() {
+//     const data = await 400;
+//     console.log('data',data);
+// })()
+
+// !(async function () {
+//   const p = Promise.reject('err');
+//   try {
+//     const d = await p;
+//     console.log('d', d);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// })();
+
+// !(async function() {
+//     const p4 = Promise.reject('err1');
+//     const d = await p4;
+//     console.log('data',d);
+// })()
+
+console.log(100);
+
+setTimeout(() => {
+    console.log(200);
+});
+
+Promise.resolve().then(() => {
+    console.log(300);
+});
+
+console.log(400);
+
+
