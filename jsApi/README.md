a0a880b202af909351a2f01e72cc522351faf3c0
dezhizhang <1541609448@qq.com>
2024-04-19 22:16:20 +0800

    ftr: 深烤贝

--- jsApi/README.md
+++ jsApi/README.md
@@ -164,3 +164,28 @@ MyPromise.race = function (promiseList) {
 };
 
 ```
+### 深拷贝
+```js
+function deepClone(obj = {}) {
+  if (typeof obj !== 'object' || obj == null) {
+    return obj;
+  }
+
+  let result;
+  if (obj instanceof Array) {
+    result = [];
+  } else {
+    result = {};
+  }
+
+  for (let key in obj) {
+    // 保证key 不是原型的属性
+    if (obj.hasOwnProperty(key)) {
+      result[key] = deepClone(obj[key]);
+    }
+  }
+
+  return result;
+}
+
+```
--- jsApi/examples/deepClone.js
+++ jsApi/examples/deepClone.js
@@ -1,13 +1,12 @@
 /*
- * :file description:
- * :name: /jsApi/src/index.js
+ * :file description: 深烤贝
+ * :name: /jsApi/examples/deepClone.js
  * :author: 张德志
  * :copyright: (c) 2024, Tungee
- * :date created: 2024-04-18 05:51:29
+ * :date created: 2024-04-19 22:04:17
  * :last editor: 张德志
- * :date last edited: 2024-04-19 22:15:24
+ * :date last edited: 2024-04-19 22:16:00
  */
-
 const obj1 = {
   age: 20,
   name: 'xxx',
@@ -23,8 +22,7 @@ obj2.address.city = 'shanghai';
 obj2.arr[0] = 'a1';
 
 console.log(obj1.address.city);
-console.log(obj2.arr);
-
+console.log(obj1.arr);
 
 function deepClone(obj = {}) {
   if (typeof obj !== 'object' || obj == null) {
@@ -39,7 +37,7 @@ function deepClone(obj = {}) {
   }
 
   for (let key in obj) {
-    //保证key 不是原型的属性
+    // 保证key 不是原型的属性
     if (obj.hasOwnProperty(key)) {
       result[key] = deepClone(obj[key]);
     }
