0433d87ee1b03d342ef94185e48ffb591f3ce33d
dezhizhang <1541609448@qq.com>
2024-04-19 21:18:24 +0800

    feat:promise

--- jsApi/README.md
+++ jsApi/README.md
@@ -28,3 +28,139 @@ Promise.resolve().then(() => {
 
 console.log(400);
 ```
+
+### 手写promise TODO 回来重点看
+```js
+class MyPromise {
+  state = 'pending';
+  value = undefined;
+  reason = undefined;
+
+  resolveCallbacks = [];
+  rejectCallbacks = [];
+
+  constructor(fn) {
+    const resolveHandler = (value) => {
+      if (this.state === 'pending') {
+        this.state = 'fulfilled';
+        this.value = value;
+        this.resolveCallbacks.forEach((fn) => fn(this.value));
+      }
+    };
+    const rejectHandler = (reason) => {
+      if (this.state === 'pending') {
+        this.state = 'rejected';
+        this.reason = reason;
+        this.resolveCallbacks.forEach((fn) => fn(this.reason));
+      }
+    };
+    try {
+      fn(resolveHandler, rejectHandler);
+    } catch (err) {
+      rejectHandler(err);
+    }
+  }
+  then(fn1, fn2) {
+    fn1 = typeof fn1 === 'function' ? fn1 : (v) => v;
+    fn2 = typeof fn2 === 'function' ? fn2 : (e) => e;
+
+    if (this.state === 'pending') {
+      const p1 = new MyPromise((resolve, reject) => {
+        this.resolveCallbacks.push(() => {
+          try {
+            const newValue = fn1(this.value);
+            resolve(newValue);
+          } catch (err) {
+            reject(err);
+          }
+        });
+
+        this.rejectCallbacks.push(() => {
+          try {
+            const newReason = fn2(this.reason);
+            reject(newReason);
+          } catch (err) {
+            reject(err);
+          }
+        });
+      });
+      return p1;
+    }
+
+    // fulfilled状态
+    if (this.state === 'fulfilled') {
+      const p1 = new MyPromise((resolve, reject) => {
+        try {
+          const newValue = fn1(this.value);
+          resolve(newValue);
+        } catch (err) {
+          reject(err);
+        }
+      });
+      return p1;
+    }
+
+    // rejcted
+    if (this.state === 'rejcted') {
+      const p1 = new MyPromise((resolve, reject) => {
+        try {
+          const newReason = fn2(this.reason);
+          reject(newReason);
+        } catch (err) {
+          reject(err);
+        }
+      });
+      return p1;
+    }
+  }
+  catch(fn) {
+    return this.then(null, fn);
+  }
+}
+
+MyPromise.resolve = function (value) {
+  return new MyPromise((resolve) => resolve(value));
+};
+
+MyPromise.reject = function (reason) {
+  return new MyPromise((resolve, reject) => reject(reason));
+};
+
+MyPromise.all = function (promiseList = []) {
+  const p1 = new Promise((resolve, reject) => {
+    const result = [];
+    const length = promiseList.length;
+    let resolveCount = 0;
+    promiseList.forEach((p) => {
+      p.then((data) => {
+        result.push(data);
+        resolveCount++;
+        if (resolveCount === length) {
+          resolve(result);
+        }
+      }).catch((err) => {
+        reject(err);
+      });
+    });
+  });
+  return p1;
+};
+
+MyPromise.race = function (promiseList) {
+  let resolved = false;
+  const p1 = new MyPromise((resolve, reject) => {
+    promiseList.forEach((p) => {
+      p.then((data) => {
+        if (!resolved) {
+          resolve(data);
+          resolved = true;
+        }
+      }).catch((err) => {
+        reject(err);
+      });
+    });
+  });
+  return p1;
+};
+
+```
--- jsApi/src/index.js
+++ jsApi/src/index.js
@@ -5,46 +5,157 @@
  * :copyright: (c) 2024, Tungee
  * :date created: 2024-04-18 05:51:29
  * :last editor: 张德志
- * :date last edited: 2024-04-18 07:39:17
+ * :date last edited: 2024-04-19 21:15:09
  */
-// async function fn1() {
-//     return 100;
-// }
 
-// const res1 = fn1();
-// console.log('res1',res1);
+class MyPromise {
+  state = 'pending';
+  value = undefined;
+  reason = undefined;
 
-// !(async function() {
-//     const data = await 400;
-//     console.log('data',data);
-// })()
+  resolveCallbacks = [];
+  rejectCallbacks = [];
 
-// !(async function () {
-//   const p = Promise.reject('err');
-//   try {
-//     const d = await p;
-//     console.log('d', d);
-//   } catch (err) {
-//     console.log(err);
-//   }
-// })();
+  constructor(fn) {
+    const resolveHandler = (value) => {
+      if (this.state === 'pending') {
+        this.state = 'fulfilled';
+        this.value = value;
+        this.resolveCallbacks.forEach((fn) => fn(this.value));
+      }
+    };
+    const rejectHandler = (reason) => {
+      if (this.state === 'pending') {
+        this.state = 'rejected';
+        this.reason = reason;
+        this.resolveCallbacks.forEach((fn) => fn(this.reason));
+      }
+    };
+    try {
+      fn(resolveHandler, rejectHandler);
+    } catch (err) {
+      rejectHandler(err);
+    }
+  }
+  then(fn1, fn2) {
+    fn1 = typeof fn1 === 'function' ? fn1 : (v) => v;
+    fn2 = typeof fn2 === 'function' ? fn2 : (e) => e;
 
-// !(async function() {
-//     const p4 = Promise.reject('err1');
-//     const d = await p4;
-//     console.log('data',d);
-// })()
+    if (this.state === 'pending') {
+      const p1 = new MyPromise((resolve, reject) => {
+        this.resolveCallbacks.push(() => {
+          try {
+            const newValue = fn1(this.value);
+            resolve(newValue);
+          } catch (err) {
+            reject(err);
+          }
+        });
 
-console.log(100);
+        this.rejectCallbacks.push(() => {
+          try {
+            const newReason = fn2(this.reason);
+            reject(newReason);
+          } catch (err) {
+            reject(err);
+          }
+        });
+      });
+      return p1;
+    }
 
-setTimeout(() => {
-    console.log(200);
+    // fulfilled状态
+    if (this.state === 'fulfilled') {
+      const p1 = new MyPromise((resolve, reject) => {
+        try {
+          const newValue = fn1(this.value);
+          resolve(newValue);
+        } catch (err) {
+          reject(err);
+        }
+      });
+      return p1;
+    }
+
+    // rejcted
+    if (this.state === 'rejcted') {
+      const p1 = new MyPromise((resolve, reject) => {
+        try {
+          const newReason = fn2(this.reason);
+          reject(newReason);
+        } catch (err) {
+          reject(err);
+        }
+      });
+      return p1;
+    }
+  }
+  catch(fn) {
+    return this.then(null, fn);
+  }
+}
+
+MyPromise.resolve = function (value) {
+  return new MyPromise((resolve) => resolve(value));
+};
+
+MyPromise.reject = function (reason) {
+  return new MyPromise((resolve, reject) => reject(reason));
+};
+
+MyPromise.all = function (promiseList = []) {
+  const p1 = new Promise((resolve, reject) => {
+    const result = [];
+    const length = promiseList.length;
+    let resolveCount = 0;
+    promiseList.forEach((p) => {
+      p.then((data) => {
+        result.push(data);
+        resolveCount++;
+        if (resolveCount === length) {
+          resolve(result);
+        }
+      }).catch((err) => {
+        reject(err);
+      });
+    });
+  });
+  return p1;
+};
+
+MyPromise.race = function (promiseList) {
+  let resolved = false;
+  const p1 = new MyPromise((resolve, reject) => {
+    promiseList.forEach((p) => {
+      p.then((data) => {
+        if (!resolved) {
+          resolve(data);
+          resolved = true;
+        }
+      }).catch((err) => {
+        reject(err);
+      });
+    });
+  });
+  return p1;
+};
+
+const p1 = new MyPromise((resolve, reject) => {
+  resolve(100);
 });
 
-Promise.resolve().then(() => {
-    console.log(300);
-});
+const p2 = MyPromise.resolve(200);
+const p3 = MyPromise.resolve(300);
 
-console.log(400);
+const result = MyPromise.race([p2, p3]);
+console.log(result);
 
+// const p11 = p1.then((data1) => {
+//   console.log('data1', data1);
+//   return data + 1;
+// });
 
+// const p12 = p11.then((data2) => {
+//   console.log('data2', data2);
+//   return data2 + 2;
+// });
