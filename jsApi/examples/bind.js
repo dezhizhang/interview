9b2c78ff8af57d2d608580041fdcbd4d9d94b9bb
dezhizhang <1541609448@qq.com>
2024-04-21 22:08:27 +0800

    feat:作用域

---
+++ jsApi/examples/bind.js
@@ -0,0 +1,9 @@
+/*
+ * :file description: 
+ * :name: /jsApi/examples/bind.js
+ * :author: 张德志
+ * :copyright: (c) 2024, Tungee
+ * :date created: 2024-04-21 22:01:22
+ * :last editor: 张德志
+ * :date last edited: 2024-04-21 22:01:22
+ */
--- jsApi/examples/作用域.js
+++ jsApi/examples/作用域.js
@@ -1,13 +1,12 @@
 /*
- * :file description:
- * :name: /jsApi/src/index.js
+ * :file description: 
+ * :name: /jsApi/examples/作用域.js
  * :author: 张德志
  * :copyright: (c) 2024, Tungee
- * :date created: 2024-04-18 05:51:29
+ * :date created: 2024-04-21 22:07:52
  * :last editor: 张德志
- * :date last edited: 2024-04-21 22:07:21
+ * :date last edited: 2024-04-21 22:07:57
  */
-
 let i, a;
 
 for (i = 0; i < 10; i++) {
