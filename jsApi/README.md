6505da9cfc851c25ee3d52670a5a2d3eac25b566
dezhizhang <1541609448@qq.com>
2024-04-28 23:12:42 +0800

    feat:判断括号是否匹配

--- jsApi/README.md
+++ jsApi/README.md
@@ -609,6 +609,48 @@ const arr1 = rotate2(arr, 3);
 console.log(arr1);
 
 ```
+### 判断括号是否匹配 
+```js
+function isMatch(left, right) {
+  if (left === '{' && right === '}') return true;
+  if (left === '[' && right === ']') return true;
+  if (left === '(' && right === ')') return true;
+
+  return false;
+}
+
+function matchBracket(str) {
+  const length = str.length;
+
+  if (length === 0) return true;
+
+  const stack = [];
+
+  const leftSymbols = '([{';
+  const rightSymbols = '}])';
+
+  for (let i = 0; i < length; i++) {
+    const s = str[i];
+    if (leftSymbols.includes(s)) {
+      stack.push(s);
+    } else if (rightSymbols.includes(s)) {
+      const top = stack[stack.length - 1];
+      if (isMatch(top, s)) {
+        stack.pop();
+      }else {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
+}
+
+const str = '([{}])';
+
+
+console.log(matchBracket(str));
+```
 
 
 
--- jsApi/src/index.js
+++ jsApi/src/index.js
@@ -5,22 +5,41 @@
  * :copyright: (c) 2024, Tungee
  * :date created: 2024-04-18 05:51:29
  * :last editor: 张德志
- * :date last edited: 2024-04-28 21:53:34
+ * :date last edited: 2024-04-28 23:09:50
  */
 
-function rotate2(arr, k) {
-  const length = arr.length;
-  if (!k || length === 0) return arr;
+function isMatch(left, right) {
+  if (left === '{' && right === '}') return true;
+  if (left === '[' && right === ']') return true;
+  if (left === '(' && right === ')') return true;
 
-  const step = Math.abs(k % length);
-
-  const part1 = arr.slice(-step);
-  const part2 = arr.slice(0, length - step);
-
-  return part1.concat(part2);
+  return false;
 }
 
-const arr = [1, 2, 3, 4, 5, 6, 7];
+function matchBracket(str) {
+  const length = str.length;
+  if (length === 0) return true;
 
-const arr1 = rotate2(arr, 3);
-console.log(arr1);
+  const stack = [];
+  const leftSymbols = '([{';
+  const rightSymbols = '}])';
+
+  for (let i = 0; i < length; i++) {
+    const s = str[i];
+    if (leftSymbols.includes(s)) {
+      stack.push(s);
+    } else if (rightSymbols.includes(s)) {
+      const top = stack[stack.length - 1];
+      if (isMatch(top, s)) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
+}
+
+const str = '([{(}])';
+
+console.log(matchBracket(str));
