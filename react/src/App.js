aa1bcb80e9a331047f5e029f4ed497059ad26a6c
dezhizhang <1541609448@qq.com>
2024-05-01 20:55:12 +0800

    feat:bind帮定this

--- react/src/App.js
+++ react/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+/*
+ * :file description:
+ * :name: /react/src/App.js
+ * :author: 张德志
+ * :copyright: (c) 2024, Tungee
+ * :date created: 2024-04-26 16:08:06
+ * :last editor: 张德志
+ * :date last edited: 2024-05-01 20:53:56
+ */
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react';
+
+class App extends React.PureComponent {
+  constructor() {
+    super();
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    console.log(this);
+  }
+  render() {
+    return <div onClick={this.handleClick}>hello</div>;
+  }
 }
 
 export default App;
