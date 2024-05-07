d73f274e3c8860d5c03696ec48a31b997b4c018b
dezhizhang <1541609448@qq.com>
2024-05-08 05:26:15 +0800

    ftr:数组偏平化

--- jsApi/examples/11数组偏平化.js
+++ jsApi/examples/11数组偏平化.js
@@ -1,33 +1,22 @@
 /*
  * :file description:
- * :name: /jsApi/src/index.js
+ * :name: /jsApi/examples/11数组偏平化.js
  * :author: 张德志
  * :copyright: (c) 2024, Tungee
- * :date created: 2024-04-18 05:51:29
+ * :date created: 2024-05-08 05:25:15
  * :last editor: 张德志
- * :date last edited: 2024-05-08 05:25:34
+ * :date last edited: 2024-05-08 05:25:21
  */
-
-
 export function flatten(arr) {
   const result = [];
   arr.forEach((item) => {
-    if(Array.isArray(item)) {
+    if (Array.isArray(item)) {
       result.push(...item);
-    }else {
-      result.push(item)
+    } else {
+      result.push(item);
     }
   });
   return result;
 }
 
-
-console.log(flatten([1,2,[3,[5],4]]));
-
-
-
-
-
-
-
-
+console.log(flatten([1, 2, [3, 4]]));
