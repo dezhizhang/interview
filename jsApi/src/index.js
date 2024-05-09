657dbf5d56892bb09508422f5b52798c2df7c05d
dezhizhang <1541609448@qq.com>
2024-05-09 23:31:06 +0800

    feat:update

--- jsApi/src/index.js
+++ jsApi/src/index.js
@@ -5,37 +5,36 @@
  * :copyright: (c) 2024, Tungee
  * :date created: 2024-04-18 05:51:29
  * :last editor: 张德志
- * :date last edited: 2024-05-09 22:44:32
+ * :date last edited: 2024-05-09 23:30:32
  */
 
-class LRUCache {
+
+class LRUCache{
   constructor(length) {
-    if (length < 1) throw new Error('无效的length');
+    if(length < 1) throw new Error('无效的length');
+
     this.length = length;
     this.data = new Map();
   }
-
-  set(key, value) {
+  set(key,value) {
     const data = this.data;
-    if (data.has(key)) {
+    if(data.has(key)) {
       data.delete(key);
     }
+    data.set(key,value);
 
-    data.set(key, value);
-
-    if (data.size > this.length) {
+    if(data.size > this.length) {
       const delKey = data.keys().next().value;
       data.delete(delKey);
     }
   }
-
   get(key) {
     const data = this.data;
     if(!data.has(key)) return null;
+
     const value = data.get(key);
 
     data.delete(key);
-
     data.set(key,value);
 
     return value;
