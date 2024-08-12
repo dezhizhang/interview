50f879c431f32477d0504976f552c41d284b1880
dezhizhang <1541609448@qq.com>
2024-08-12 16:26:51 +0800

    feat:调整最小堆

--- min-react/package/shared/src/SchedulerMinHeap.ts
+++ min-react/package/shared/src/SchedulerMinHeap.ts
@@ -14,7 +14,26 @@ export function peek<T extends Node>(heap: Heap<T>): T | null {
 export function push<T extends Node>(heap: Heap<T>, node: T): void {
   // 把node放在堆最后
   heap.push(node);
-  // 调整最小堆
+  // 调整最小堆，从小往上调整最小堆
+  const index = heap.length;
+
+  siftUp(heap, node, index);
+}
+
+// 从下往上调整最小堆
+function siftUp<T extends Node>(heap: Heap<T>, node: T, i: number): void {
+  let index = i;
+  while (index > 0) {
+    const parentIndex = (index - 1) >>> 1;
+    const parent = heap[parentIndex];
+    if (compare(parent, node) > 0) {
+      heap[parentIndex] = node;
+      heap[index] = parent;
+      index = parentIndex;
+    } else {
+      return;
+    }
+  }
 }
 
 // 比较元素
