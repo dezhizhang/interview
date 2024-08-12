481bfa36e8992cd0289ba409eeb310588109699c
dezhizhang <1541609448@qq.com>
2024-08-12 16:48:49 +0800

    feat:最小堆

--- min-react/package/shared/src/SchedulerMinHeap.ts
+++ min-react/package/shared/src/SchedulerMinHeap.ts
@@ -36,6 +36,53 @@ function siftUp<T extends Node>(heap: Heap<T>, node: T, i: number): void {
   }
 }
 
+// 删除堆顶元素
+export function pop<T extends Node>(heap: Heap<T>): T | null {
+  if (heap.length === 0) {
+    return null;
+  }
+  const first = heap[0];
+  const last: any = heap.pop();
+
+  if (first != last) {
+    heap[0] = last;
+    siftDown(heap, last, 0);
+  }
+
+  return first;
+}
+
+// 向下比较交换位置
+function siftDown<T extends Node>(heap: Heap<T>, node: T, i: number): void {
+  let index = i;
+  const length = heap.length;
+  const halfLength = length >>> 1;
+  while (index < halfLength) {
+    const leftIndex = (index + 1) * 2 - 1;
+    const left = heap[leftIndex];
+    const rightIndex = leftIndex + 1;
+    const right = heap[rightIndex];
+
+    if (compare(left, node) < 0) {
+      if (rightIndex < length && compare(right, left) < 0) {
+        heap[index] = right;
+        heap[rightIndex] = node;
+        index = rightIndex;
+      } else {
+        heap[index] = left;
+        heap[leftIndex] = node;
+        index = leftIndex;
+      }
+    } else if (rightIndex < length && compare(right, node) < 0) {
+      heap[index] = right;
+      heap[rightIndex] = node;
+      index = rightIndex;
+    } else {
+      break;
+    }
+  }
+}
+
 // 比较元素
 function compare(a: Node, b: Node) {
   const diff = a.sortIndex - b.sortIndex;
