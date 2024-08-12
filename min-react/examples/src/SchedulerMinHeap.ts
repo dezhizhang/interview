43008783c8d81f07d41e8d20587a3a9a41066d1a
dezhizhang <1541609448@qq.com>
2024-08-12 15:57:36 +0800

    feat:最小堆算法

--- min-react/examples/src/SchedulerMinHeap.ts
+++ min-react/examples/src/SchedulerMinHeap.ts
@@ -10,6 +10,13 @@ export function peek<T extends Node>(heap: Heap<T>): T | null {
   return heap.length === 0 ? null : heap[0];
 }
 
+// 给堆添加元素
+export function push<T extends Node>(heap: Heap<T>, node: T): void {
+  // 把node放在堆最后
+  heap.push(node);
+  // 调整最小堆
+}
+
 // 比较元素
 function compare(a: Node, b: Node) {
   const diff = a.sortIndex - b.sortIndex;
