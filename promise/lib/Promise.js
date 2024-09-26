1239d9a6588bec7b11fdab4e2c49622fbad95a45
dezhizhang <1541609448@qq.com>
2024-09-27 05:09:30 +0800

    feat: promise

--- promise/lib/Promise.js
+++ promise/lib/Promise.js
@@ -5,25 +5,27 @@
  * :copyright: (c) 2024, Xiaozhi
  * :date created: 2024-09-24 21:27:30
  * :last editor: 张德志
- * :date last edited: 2024-09-24 22:36:56
+ * :date last edited: 2024-09-26 05:07:07
  */
+const PENDING = "pending";
+const RESOLVED = "resolved";
+const REJECTED = "rejected";
 
 class MyPromise {
   constructor(excutor) {
-    this.status = "pending";
+    this.status = PENDING;
     this.data = undefined;
     this.callbacks = [];
     let _this = this;
     function resolve(value) {
-      if (that.status !== "pending") return;
+      if (_this.status !== PENDING) return;
 
-      _this.status = "resolved";
+      _this.status = RESOLVED;
       _this.data = value;
 
       if (_this.callbacks.length > 0) {
         // 如果有待执行的callback函数，立即异步执行回调函数onResolved
-        this.callbacks.forEach((obj) => {
-          debugger;
+        _this.callbacks.forEach((obj) => {
           setTimeout(() => {
             obj.onResolved(value);
           });
@@ -32,7 +34,8 @@ class MyPromise {
     }
 
     function reject(reason) {
-      if (_this.status !== "pending") _this.status = "rejected";
+      if (_this.status !== PENDING) return;
+      _this.status = REJECTED;
       _this.data = reason;
 
       if (_this.callbacks.length > 0) {
@@ -51,10 +54,93 @@ class MyPromise {
       reject(error);
     }
   }
+
   then(onResolved, onRejected) {
-    this.callbacks.push({
-      onResolved,
-      onRejected,
+    let _this = this;
+
+    onResolved =
+      typeof onResolved === "function" ? onResolved : (value) => value;
+    onRejected =
+      typeof onRejected === "function"
+        ? onRejected
+        : (reason) => {
+            throw reason;
+          };
+
+    return new MyPromise((resolve, reject) => {
+      function handler(callback) {
+        try {
+          const result = callback(_this.data);
+          if (result instanceof Promise) {
+            result.then(resolve, reject);
+          } else {
+            resolve(result);
+          }
+        } catch (error) {
+          reject(error);
+        }
+      }
+
+      if (_this.status === PENDING) {
+        handler(onResolved);
+      } else if (_this.status === REJECTED) {
+        setTimeout(() => {
+          handler(onRejected);
+        });
+      } else {
+        _this.callbacks.push({
+          onResolved() {
+            handler(onResolved);
+          },
+          onRejected() {
+            handler(onRejected);
+          },
+        });
+      }
+    });
+  }
+  catch(onRejected) {
+    return this.then(undefined, onRejected);
+  }
+
+  static resolve(value) {
+    return new MyPromise((resolve, reject) => {
+      if (value instanceof MyPromise) {
+        value.then(resolve, reject);
+      } else {
+        resolve(value);
+      }
+    });
+  }
+
+  static reject(reason) {
+    return new MyPromise((resolve, reject) => {
+      reject(reason);
+    });
+  }
+
+
+  static all(promises) {
+    const resolvedCount = 0;
+    const values = new Array(promises.length);
+
+    return new Promise((resolve, reject) => {
+      promises.forEach((p, index) => {
+        p.then(
+          (value) => {
+            resolvedCount++;
+            values[index] = value;
+
+            // 如果全部成功了,将return的promise改成成功
+            if (resolvedCount === promises.length) {
+              resolve(values);
+            }
+          },
+          (reason) => {
+            reject(reason);
+          }
+        );
+      });
     });
   }
 }
--- promise/src/index.js
+++ promise/src/index.js
@@ -5,32 +5,49 @@
  * :copyright: (c) 2024, Xiaozhi
  * :date created: 2024-09-23 15:42:55
  * :last editor: 张德志
- * :date last edited: 2024-09-24 22:43:40
+ * :date last edited: 2024-09-26 06:54:45
  */
-import MyPromise from "../lib/Promise";
+setTimeout(() => {
+  console.log('0');
+},0);
 
-const p = new MyPromise((resolve, reject) => {
-  resolve("hello");
-  console.log('reject状态改变了')
+new Promise((resolve,reject) => {
+  console.log('1');
+  resolve();
+}).then(() => {
+  console.log('2');
+  return new Promise((resolve,reject) => {
+    console.log('3');
+    resolve();
+  }).then(() => {
+    console.log('4');
+  }).then(() => {
+    console.log('5');
+  })
+}).then(() => {
+  console.log('6');
 });
 
-p.then(
-  (value) => {
-    console.log('onResolved1()',value)
-  },
-  (reason) => {
-    console.log("onRejected1()", reason);
-  }
-);
 
-p.then(
-  (value) => {
-    console.log('onResolved2()',value)
-  },
-  (reason) => {
-    console.log("onRejected2()", reason);
-  }
-);
+new Promise((resolve,reject) => {
+  console.log('7');
+  resolve();
+}).then(() => {
+  console.log('8');
+});
+
+
+//1
+// index.js:33 7
+// index.js:18 2
+// index.js:20 3
+// index.js:36 8
+// index.js:23 4
+// index.js:25 5
+// index.js:28 6
+// index.js:11 0
+// 1,3,7,2,4,5,6,8,0;
+
 
 
 
