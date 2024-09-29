66942782339c1089e20659f47681637f15c662ac
dezhizhang <1541609448@qq.com>
2024-09-29 10:28:48 +0800

    feat: 项目初始化

---
+++ tailwindcss/tailwind.config.js
@@ -0,0 +1,17 @@
+/*
+ * :file description: 
+ * :name: /tailwindcss/tailwind.config.js
+ * :author:张德志
+ * :copyright: (c) 2024, Xiaozhi
+ * :date created: 2024-09-29 09:49:14
+ * :last editor: 张德志
+ * :date last edited: 2024-09-29 09:56:12
+ */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
