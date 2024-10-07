06c459f5dd00f35f6e37fa90c7d947906d6ae7cf
dezhizhang <1541609448@qq.com>
2024-10-07 16:35:09 +0800

    feat: 职责链模式

--- desigin/src/index.js
+++ desigin/src/index.js
@@ -1,25 +1,52 @@
-
-class NumberIterator {
-  constructor(numbers) {
-    this.numbers = numbers;
-    this.index = 0;
-  }
-  // next 方法用于获取集合中的下一个元素
-  next() {
-    if (this.index < this.numbers.length) {
-      return { value: this.numbers[this.index++], done: false };
-    } else {
-      return { value: undefined, done: true };
+// 处理者抽象类
+class Handler {
+    constructor(successor = null) {
+        this.successor = successor; // 下一个处理者
     }
-  }
+
+    handleRequest(request) {
+        if (this.successor) {
+            this.successor.handleRequest(request); // 传递请求给下一个处理者
+        }
+    }
 }
 
-const numbers = [1, 2, 3, 4, 5];
-const iterator = new NumberIterator(numbers);
-
-// 使用迭代器遍历数组
-let result = iterator.next();
-while (!result.done) {
-  console.log(result.value);
-  result = iterator.next();
+// 具体处理者类：经理
+class Manager extends Handler {
+    handleRequest(request) {
+        if (request === 'leave' || request === 'overtime') {
+            console.log('Manager approved the request: ' + request);
+        } else {
+            super.handleRequest(request); // 继续传递请求
+        }
+    }
 }
+
+// 具体处理者类：总监
+class Director extends Handler {
+    handleRequest(request) {
+        if (request === 'budget') {
+            console.log('Director approved the request: ' + request);
+        } else {
+            super.handleRequest(request); // 继续传递请求
+        }
+    }
+}
+
+// 具体处理者类：CEO
+class CEO extends Handler {
+    handleRequest(request) {
+        console.log('CEO approved the request: ' + request);
+    }
+}
+
+// 创建职责链
+const ceo = new CEO();
+const director = new Director(ceo);
+const manager = new Manager(director);
+
+// 客户端发起请求
+manager.handleRequest('leave');   // 输出: Manager approved the request: leave
+manager.handleRequest('budget');   // 输出: Director approved the request: budget
+manager.handleRequest('overtime'); // 输出: Manager approved the request: overtime
+manager.handleRequest('other');    // 输出: CEO approved the request: other
