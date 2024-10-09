25aea7fbae46d73c8a5fb752d7e092c4ad7fd0a8
dezhizhang <1541609448@qq.com>
2024-10-09 18:08:58 +0800

    feat:opt

---
+++ performance/src/index.js
@@ -0,0 +1,26 @@
+/*
+ * :file description:
+ * :name: /performance/src/index.js
+ * :author:张德志
+ * :copyright: (c) 2024, Xiaozhi
+ * :date created: 2024-10-08 21:55:15
+ * :last editor: 张德志
+ * :date last edited: 2024-10-09 16:00:32
+ */
+
+// function fn() {
+//   let str = "";
+//   for (let i = 0; i < 1000; i++) {
+//     str += i;
+//   }
+// }
+
+// fn();
+
+// const btn = document.getElementById("btn");
+
+// function hashClassName(ele, cls) {
+//   return ele.className(cls);
+// }
+
+// console.log(hashClassName(btn, "btn"));
---
+++ performance/webpack.config.js
@@ -0,0 +1,51 @@
+/*
+ * :file description: 
+ * :name: /interview/performance/webpack.config.js
+ * :author:张德志
+ * :copyright: (c) 2024, Xiaozhi
+ * :date created: 2024-10-08 21:52:59
+ * :last editor: 张德志
+ * :date last edited: 2024-10-08 21:53:25
+ */
+/*
+ * :file description:
+ * :name: /jsApi/webpack.config.js
+ * :author: 张德志
+ * :copyright: (c) 2024, Tungee
+ * :date created: 2024-04-18 05:48:41
+ * :last editor: 张德志
+ * :date last edited: 2024-09-24 19:47:43
+ */
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'build.[contenthash].js',
+  },
+  devServer: {
+    port: 8002,
+    open: false,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader'],
+      },
+      {
+        test: /\.glsl$/,
+        use: ['webpack-glsl-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      inject: 'body',
+    }),
+  ],
+};
