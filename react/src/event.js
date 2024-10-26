ab8a068687724f278527271bafafe26d2c86e899
dezhizhang <1541609448@qq.com>
2024-10-26 09:19:24 +0800

    feat:合成事件

--- react/src/event.js
+++ react/src/event.js
@@ -5,7 +5,7 @@
  * :copyright: (c) 2024, Xiaozhi
  * :date created: 2024-10-26 06:50:04
  * :last editor: 张德志
- * :date last edited: 2024-10-26 07:21:48
+ * :date last edited: 2024-10-26 09:11:26
  */
 
 import { updateQueue } from "./component";
@@ -20,6 +20,28 @@ export default function addEvent(dom, eventType, handler) {
   }
 }
 
+
+function createBaseEvent(nativeEvent) {
+  let syntheticBaseEvent = {};
+  for(let key in nativeEvent) {
+    syntheticBaseEvent[key] = nativeEvent[key];
+  }
+  syntheticBaseEvent.nativeEvent = nativeEvent;
+  // 兼容默认事件
+  syntheticBaseEvent.preventDefault = preventDefault
+  return syntheticBaseEvent;
+}
+
+
+// 处理默认事件
+function preventDefault(event){
+  if(!event) {
+    window.event.returnValue = false;
+  }
+  event && event.preventDefault();
+};
+
+
 // 将dom上的事件放在document事件上
 function dispatchEvent(event) {
   const { target, type } = event;
@@ -29,8 +51,10 @@ function dispatchEvent(event) {
   const { store } = target;
 
   const handler = store && store[eventType];
+  // 合成事件对像
+  const SyntheticBaseEvent = createBaseEvent(event);
   updateQueue.isBatchData = true;
-  handler && handler(event);
+  handler && handler(SyntheticBaseEvent);
   updateQueue.isBatchData = false;
   updateQueue.batchUpdate();
 }
