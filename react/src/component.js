c72c34e6e1d9ffff26631cb15aaffeb2b74b767a
dezhizhang <1541609448@qq.com>
2024-10-28 11:26:57 +0800

    feat:实现组件更新

--- react/src/component.js
+++ react/src/component.js
@@ -5,12 +5,64 @@
  * :copyright: (c) 2024, Xiaozhi
  * :date created: 2024-10-25 20:33:32
  * :last editor: 张德志
- * :date last edited: 2024-10-28 08:56:14
+ * :date last edited: 2024-10-28 11:26:11
  */
+
+// 实现组件更新
+function shouldUpdate(classInstance,nextState) {
+    
+}
+
+class Updater {
+  constructor(classInstance) {
+    // 保存类实例
+    this.classInstance = classInstance;
+    // 保存数据
+    this.peddingState = [];
+  }
+  // 添加数据
+  addState(partialState) {
+    this.peddingState.push(partialState);
+
+    this.emitUpdate();
+  }
+
+  // 获取最新数据
+  getState() {
+    const { classInstance, peddingState } = this;
+    let { state } = classInstance;
+    peddingState.forEach((nestState) => {
+      state = { ...state, nestState };
+    });
+
+    // 清空数据
+    peddingState.length = 0;
+    // 返回最新数据
+    return state;
+  }
+
+  // 更新组件
+  updateComponent() {
+    const { classInstance, peddingState } = this;
+    if (peddingState.length > 0) {
+      shouldUpdate(classInstance, this.getState());
+    }
+  }
+  // 更新数据
+  emitUpdate() {
+    this.updateComponent();
+  }
+}
+
 class Component {
   static isReactComponent = true;
   constructor(props) {
     this.props = props;
+    this.state = {};
+    this.updater = new Updater(this);
+  }
+  setState(partialState) {
+    this.updater.addState(partialState);
   }
 }
 
--- react/src/index.js
+++ react/src/index.js
@@ -5,35 +5,30 @@
  * :copyright: (c) 2024, Xiaozhi
  * :date created: 2024-07-25 22:20:46
  * :last editor: 张德志
- * :date last edited: 2024-10-28 08:53:40
+ * :date last edited: 2024-10-28 11:08:10
  */
 import React from "./react";
 import ReactDOM from "./react-dom";
 
-// function App({name}) {
-//   return React.createElement('h1',{
-//     style:{
-//       color:'red'
-//     }
-//   },'hello world')
-// }
-
-// const element = <App name="hello world"/>;
-
-// console.log('element',element);
-
-class App extends React.Component{
+class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {
+      num: 1,
+    };
   }
   render() {
-    return <div style={{color:'red'}}>hello {this.props.name}</div>
+    return (
+      <div>
+        <div style={{ color: "red" }}>{this.state.num}</div>
+        <button onClick={() => this.setState({ num: this.state.num + 1 })}>
+          +
+        </button>
+      </div>
+    );
   }
 }
 
+console.log(<App />);
 
-console.log(<App/>)
-
-
-ReactDOM.render(<App name={"world"}/>,document.querySelector('#root'));
+ReactDOM.render(<App name={"world"} />, document.querySelector("#root"));
